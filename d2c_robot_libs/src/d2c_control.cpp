3051afeb3999b4730e312630e5eeb4ff90bf561a
junghs1040 <junghs1040@naver.com>
2021-11-03 22:20:47 +0900

    commit 1

---
+++ d2c_robot_libs/src/d2c_control.cpp
@@ -0,0 +1,4 @@
+
+
+// Code description
+// Needed 1. Dynamixel control 2. Computer vision 3. Camera control 
---
+++ d2c_robot_libs/src/robot_node.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "d2c_robot_libs/robot_node.hpp"
+#include "ros/ros.h"
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "robot_node");
+    
+    ros::NodeHandle nh("");
+    ros::NodeHandle nh_priv("~");
+    
+    ros::spin();
+    return 0;
+}
